f8734c47ebf3f067b5cbd997e8483c547167f81a
DavidSantana872 <ds6224244@gmail.com>
2023-03-28 06:23:58 -0500

    seccione codigo funcion (Procesamiento_img, Ruta_Destino y agregue logica de comunicacion entre procesamiento)

--- VisualizacionTiempoReal/consumers.py
+++ VisualizacionTiempoReal/consumers.py
@@ -3,31 +3,9 @@ from channels.generic.websocket import WebsocketConsumer
 import cv2
 import base64
 import numpy as np 
-
+from homepage import views
 class VisualizacionTiempoRealConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-        self.send(text_data=json.dumps({
-            'type': 'connection_established',
-            'message': 'you are now connected'
-        }))
-        # captura de video 
-        url = "ip de video "
-        captura = cv2.VideoCapture(url)
-
-        #rango de colores 
-        lower_blue = np.array([100, 50, 50]) 
-        upper_blue = np.array([130, 255, 255])
-
-        lower_red = np.array([175, 100, 20], np.uint8)
-        upper_red = np.array([179, 255, 255], np.uint8)
-        
-        lower_green = np.array([40, 50, 50]) 
-        upper_green = np.array([70, 255, 255])
-
-        lower_yellow = np.array([20, 50, 50])
-        upper_yellow = np.array([60, 255, 255])
-
+    def Procesamiento_img(self, captura, lower_color, upper_color):
         while(captura.isOpened()):
             ret, imagen = captura.read()
             if ret == True:
@@ -35,13 +13,13 @@ class VisualizacionTiempoRealConsumer(WebsocketConsumer):
                 # Convertir la imagen codificada en bytes a una cadena base64 que se puede incrustar en la página web
                 img_base64 = base64.b64encode(img_encoded).decode('utf-8')
                 
-                #enviar por el socket la imagen original
+                # Enviar por el socket la imagen original
                 self.send(text_data=json.dumps({
                     'type': 'img',
                     'message': img_base64
                 }))
                 hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
-                mask = cv2.inRange(hsv, lower_green, upper_green)
+                mask = cv2.inRange(hsv, lower_color, upper_color)
                 # quitar ruido 
                 kernel = np.ones((15,15),np.uint8)
                 mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -60,17 +38,111 @@ class VisualizacionTiempoRealConsumer(WebsocketConsumer):
                     cnt = max(contours, key=cv2.contourArea)
                     x, y, w, h = cv2.boundingRect(cnt)
                    #print(w,h)
-                    cv2.rectangle(imagen, (x, y), (x + w, y + h), (0, 0, 0), 2)
+                    cv2.rectangle(imagen, (x, y), (x + w, y + h), (20, 255,0), 2)
+                    cv2.putText(imagen, f'(Detectado)', (x, y-10), cv2.FONT_HERSHEY_DUPLEX, 1, (20, 255, 0), 2)
                     img_encoded = cv2.imencode('.jpg', imagen)[1]
                     # Convertir la imagen codificada en bytes a una cadena base64 que se puede incrustar en la página web
                     img_base64 = base64.b64encode(img_encoded).decode('utf-8')
                     self.send(text_data=json.dumps({
                         'type': 'img_procesada',
-                        'message': img_base64
+                        'color' : upper_color
                     }))
                     # Mostrar las coordenadas del rectángulo sobre la imagen original
                     #cv2.putText(imagen, f'(VERDE)', (x, y-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 2)
+    def Ruta_destino(self, ruta_color, captura):
+        # ruta_color es el numero de la prioridad 
+        # es decir que los de prioridad 1 van por la ruta color amarillo...
+        if(ruta_color == 1):
+            lower_yellow = np.array([20, 50, 50])
+            upper_yellow = np.array([60, 255, 255])
+            # el carrito debe ir y venir (hay que modificar algo pq esta solo para renocimiento procesamiento_img)
+            # o crear otra funcion
+            self.Procesamiento_img(captura, lower_yellow, upper_yellow)
+            self.send(text_data=json.dumps({
+                    'type': 'ColorRuta',
+                    'message': "A"
+                }))
 
+        elif(ruta_color == 2):
+            lower_green = np.array([40, 50, 50]) 
+            upper_green = np.array([70, 255, 255])
+            self.Procesamiento_img(captura, lower_green, upper_green)
+            self.send(text_data=json.dumps({
+                    'type': 'ColorRuta',
+                    'message': "V"
+                }))
+
+        elif(ruta_color == 3):
+            lower_red = np.array([175, 100, 20], np.uint8)
+            upper_red = np.array([179, 255, 255], np.uint8)
+            self.Procesamiento_img(captura, lower_red, upper_red)
+            self.send(text_data=json.dumps({
+                    'type': 'ColorRuta',
+                    'message': "R"
+                }))
+            
+
+    def connect(self):
+        self.accept()
+        self.send(text_data=json.dumps({
+            'type': 'connection_established',
+            'message': 'you are now connected'
+        }))
+        # captura de video 
+        url = "http://192.168.1.135:8080/video"
+        captura = cv2.VideoCapture(url)
+        # amarillo verde rojo 
+       
+        i = 1
+        while(i <= 3):
+            if(int(views.prioridad_amarillo) == i):
+                print(f"{i} amarillo")
+                lower_yellow = np.array([20, 50, 50])
+                upper_yellow = np.array([60, 255, 255])
+                self.send(text_data=json.dumps({
+                    'type': 'color_detectando',
+                    'message': "A"
+                }))
+                # aplicando procesamiento para tomar el paquete 
+                self.Procesamiento_img(captura, lower_yellow, upper_yellow)
+                # aplicar el procesamiento para ir a dejar el paquete 
+                color_ruta = views.destino_amarillo
+                # manda a buscar su ruta 
+                self.Ruta_destino(color_ruta, captura)
+
+            elif(int(views.prioridad_verde) == i):
+                print(f"{i} verde")        
+                lower_green = np.array([40, 50, 50]) 
+                upper_green = np.array([70, 255, 255])
+                self.send(text_data=json.dumps({
+                    'type': 'color_detectando',
+                    'message': "V"
+                }))
+                self.Procesamiento_img(captura, lower_green, upper_green)
+
+                # soluciones
+                # 1- esperar una señal del arduino que ya termino
+                # 2- mandar todo los datos directamente (no viable kreo)
+
+            elif(int(views.prioridad_rojo) == i):
+                print(f"{i} rojo")
+                lower_red = np.array([175, 100, 20], np.uint8)
+                upper_red = np.array([179, 255, 255], np.uint8)
+                self.send(text_data=json.dumps({
+                    'type': 'color_detectando',
+                    'message': "R"
+                }))
+                self.Procesamiento_img(captura, lower_red, upper_red)
+                
+            i = i + 1
+        
+
+        
+        
+        
+        
+
+        
             
                 
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect
 
+#--------------------------
+# inicializacion
+prioridad_amarillo = 0 
+prioridad_verde = 0
+prioridad_rojo = 0
+
+destino_amarillo = 0 
+destino_verde = 0 
+destino_rojo = 0
+#--------------------------
 # Create your views here.
 def home(request):
     return render(request, 'homepage/index.html')
@@ -10,28 +20,35 @@ def PrioridadPaquete(request):
 
 def DestinoPaquete(request):
      if request.method == 'POST':
-        select1_value = request.POST.get('select1')
-        select2_value = request.POST.get('select2')
-        select3_value = request.POST.get('select3')
+
+        # ---------------------------
+        # Declarar valores globales en este modulo
+        global prioridad_amarillo 
+        global prioridad_verde 
+        global prioridad_rojo
+        # ---------------------------
+        prioridad_amarillo = request.POST.get('select1')
+        prioridad_verde = request.POST.get('select2')
+        prioridad_rojo = request.POST.get('select3')
+
         return render(request, 'homepage/ColorDestino.html')
     
-
-        # hacer algo con los valores de los selectores
      else:
         # renderizar la plantilla del formulario
         return render(request, 'homepage/ColorDestino.html')
 
 def IniciarProceso(request):
     if request.method == 'POST':
-        select4_value = request.POST.get('select1')
-        select5_value = request.POST.get('select2')
-        select6_value = request.POST.get('select3')
-        return render(request, 'homepage/Start.html')
-    
+        global destino_amarillo
+        global destino_verde
+        global destino_rojo
 
-        # hacer algo con los valores de los selectores
+        destino_amarillo = request.POST.get('select1')
+        destino_verde = request.POST.get('select2')
+        destino_rojo = request.POST.get('select3')
+ 
+        return render(request, 'homepage/Start.html')
     else:
         # renderizar la plantilla del formulario
         return render(request, 'homepage/Start.html')
-
 
